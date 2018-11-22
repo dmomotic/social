e6cb519851738404d7179fbcf2e3e05cb24a6cfc
Diego Momotic <diegomomotic@gmail.com>
2018-11-22 16:45:27 -0600

    Feature crear estados con TDD

---
+++ app/Models/Status.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Status extends Model
+{
+    //Indicamos que no deseamos proteccion para asignaciones masivas de ningun campo
+    protected $guarded = [];
+}
---
+++ tests/Feature/CreateStatusTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CreateStatusTest extends TestCase
+{
+    //Ejecutamos migraciones ya que nuestra base de datos para pruebas se carga en memoria
+    use RefreshDatabase; 
+
+    /** @test **/
+    public function an_authenticated_user_can_create_statuses()
+    {
+        //Para evitar que laravel maneje las excepciones
+        $this->withoutExceptionHandling();
+
+        //1.Given => Teniendo un usuario autenticado
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+
+        //2.When => Cuando hace un post request a status
+        $this->post(route('statuses.store'), ['body' => 'Mi primer status']);
+        
+        //3.Then => Entonces veo un nuevo estado en la base de datos
+        $this->assertDatabaseHas('statuses', [
+            'user_id' => $user->id,
+            'body' => 'Mi primer status'
+        ]);
+    }
+}
